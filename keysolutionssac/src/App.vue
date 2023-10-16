e31ba6bab11af1ad9af63bec15927be5de655cf9
PestanaS273 <118370117+PestanaS273@users.noreply.github.com>
2023-10-16 19:33:51 +0200

    Create project, header footer and main

---
+++ keysolutionssac/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <HeaderVue />
+    <div class="body">
+      <router-view />
+      <landing-page/>
+    </div>
+    <FooterVue />
+  </div>
+</template>
+
+<script>
+import HeaderVue from './components/HeaderVue.vue'
+import FooterVue from './components/FooterVue.vue'
+import LandingPage from './views/LandingPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HeaderVue,
+    FooterVue,
+    LandingPage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 0px;
+}
+
+.body {
+  margin-top: 50px;
+  margin-bottom: 50px;
+}
+
+</style>
+
---
+++ keysolutionssac/src/components/FooterVue.vue
@@ -0,0 +1,71 @@
+<template>
+    <footer>
+        <div class="footer-container">
+            <div class="footer-column">
+                <nav>
+                    <ul>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/about">About Us</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="footer-column">
+                <nav>
+                    <ul>
+                        <li><a href="https://www.facebook.com/your-facebook-page">Facebook</a></li>
+                        <li><a href="https://www.twitter.com/your-twitter-page">Twitter</a></li>
+                        <li><a href="https://www.instagram.com/your-instagram-page">Instagram</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'FooterVue'
+}
+</script>
+<style scoped>
+    footer {
+        background-color: transparent;
+        color: #ccc;
+        padding: 1rem;
+        border-top: 2px solid #ccc;
+    }
+
+    .footer-container {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .footer-column {
+        width: 50%;
+    }
+
+    nav ul {
+        list-style: none;
+        margin: 0;
+        padding: 0;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    nav ul li {
+        margin-bottom: 1rem;
+    }
+
+    a {
+        color: darkgrey;
+        text-decoration: none;
+    }
+
+    a:hover {
+        color: darkgrey;
+        text-decoration: none;
+    }
+</style>
