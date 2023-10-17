27e5b26ecdef6fe0a3e21853d68563bbb7b1a01b
PestanaS273 <118370117+PestanaS273@users.noreply.github.com>
2023-10-17 06:10:15 +0200

    Update project

--- keysolutions/README.md
+++ keysolutions/README.md
@@ -1,4 +1,4 @@
-# keysolutionssac
+# keysolutions
 
 ## Project setup
 ```
---
+++ keysolutions/src/views/ContactView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <!-- Your contact form goes here -->
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ContactView',
+}
+</script>
+
+<style scoped>
+/* Your custom styles go here */
+</style>
